ffd921f3a5c20f640ba5e3694ba8a6f019efa171
Kaviya007 <107938448+Kaviya007@users.noreply.github.com>
2022-06-25 13:31:50 +0530

    Add files via upload

---
+++ Scripts/Script1.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="angular.min.js" />
+//   1. ng-repeat directive in Angular
+/*
+var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var employees = [
+            { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000 },
+            { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000 },
+            { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000 },
+            { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000 },
+            { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
+        ];
+
+        $scope.employees = employees;
+    });
+    */
+//         2. Nesting ng-repeat with an example
+var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var countries = [
+            {
+                name: "UK",
+                cities: [
+                    { name: "London" },
+                    { name: "Birmingham" },
+                    { name: "Manchester" }
+                ]
+            },
+            {
+                name: "USA",
+                cities: [
+                    { name: "Los Angeles" },
+                    { name: "Chicago" },
+                    { name: "Houston" }
+                ]
+            },
+            {
+                name: "India",
+                cities: [
+                    { name: "Hyderabad" },
+                    { name: "Chennai" },
+                    { name: "Mumbai" }
+                ]
+            }
+        ];
+
+        $scope.countries = countries;
+    });
